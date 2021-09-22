679917961a1f611c48eb211dc7da4bd45ef77eba
soujinko <soujinko@naver.com>
2021-09-22 20:39:53 +0900

    add card add form component

--- src/components/card_edit_form copy/card_edit_form.jsx
+++ src/components/card_edit_form copy/card_edit_form.jsx
@@ -4,22 +4,22 @@ import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css'
 
 const CardEditForm = ({card}) => {
+
     const {name, company, title, email, message, theme, fileName, fileURL} = card;
     const onSubmit = () => {
-
     }
     return(
         <form className={styles.form}>
-            <input className={styles.input} type="text" name='name' value={name} />
-            <input className={styles.input} type="text" name='company' value={company} />
-            <select className={styles.select} name="theme" value={theme} >
+            <input ref={nameRef} className={styles.input} type="text" name='name' value={name} />
+            <input ref={companyRef} className={styles.input} type="text" name='company' value={company} />
+            <select ref={themeRef} className={styles.select} name="theme" value={theme} >
                 <option value="light">Light</option>
                 <option value="dark">Dark</option>
                 <option value="colorful">Colorful</option>
             </select>
-            <input className={styles.input} type="text" name='title' value={title} />
-            <input className={styles.input} type="text" name='email' value={email} />
-            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <input ref={titleRef} className={styles.input} type="text" name='title' value={title} />
+            <input ref={emailRef} className={styles.input} type="text" name='email' value={email} />
+            <textarea ref={messageRef} className={styles.textarea} name="message" value={message}></textarea>
             <div className={styles.fileInput}>
             <ImageFileInput />
             </div>
